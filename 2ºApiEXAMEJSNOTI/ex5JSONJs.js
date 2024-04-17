e1ed538a4fa2a4bd5049c243713c011911087ee5
Angel <angelmartinezlago@gmail.com>
2024-04-17 17:10:14 +0200

    Subir

--- 2ºApiEXAMEJSNOTI/ex5JSONJs.js
+++ 2ºApiEXAMEJSNOTI/ex5JSONJs.js
@@ -12,7 +12,46 @@ var vehiculoInfo = {
     email: "juan@example.com"
 };
 
+let arrayVehiculo = new array();
 
+for(let i=0;i<2;i++){
+    let marca1 = prompt("Marca:");
+    let modelo1 = promt("Modelo:");
+    let numBastidor1 = promt("Bastidor:");
+    let cilindrada1 = promt("Cilindrada:");
+    let numPuertas1 = promt("N Portas:");
+    let color1 = promt("Color:");
+    let nombre1 = prompt("Nome dono:");
+    let apellido1 = prompt("Apelido:");
+    let direccion1 = prompt("Direccion:");
+    let telefono1 = prompt("Telefono:");
+    let email1 = prompt("Email:");
+
+    let vehiculo = new vehiculoInfo();
+    vehiculo.marca = marca1;
+    vehiculo.modelo = modelo1;
+    vehiculo.numBastidor = numBastidor1;
+    vehiculo.cilindrada = cilindrada1;
+    vehiculo.numPuertas = numPuertas1;
+    vehiculo.color = color1;
+    vehiculo.nombre = nombre1;
+    vehiculo.apellido = apellido1;
+    vehiculo.direccion = direccion1;
+    vehiculo.telefono = telefono1;
+    vehiculo.email = email1;
+    
+    arrayVehiculo.push(vehiculo);
+}
+
+
+
+
+
+//Volcar nun for aparte
+
+for(let i = 0; i<2;i++){
+    localStorage.setItem(i,JSON.stringify.apply(arrayVehiculo[i]));
+}
 localStorage.setItem('vehiculoInfo', JSON.stringify(vehiculoInfo));
 
 
